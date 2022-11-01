d640c7fdbc94d2f06383d872ba8b38e186119754
scziep <sydniecziep@gmail.com>
2022-11-01 11:07:50 -0600

    first commit

---
+++ Game/HiddenWord.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace unit03_jumper
+{
+    public class HiddenWord{
+        public string hiddenword;
+        List<char> answer = new List<char>();
+        public List<char> guess = new List<char>();
+
+        public HiddenWord(){
+            
+        }
+
+        public string pullWord(){
+            string chosenWord = "chocolate";
+            return chosenWord;
+        }
+
+        public void listWord(string ripWord){
+            answer.AddRange(ripWord.ToLower());
+        }
+
+        public void createHiddenWord(){
+            foreach (int i in answer){
+                guess.Add('_');
+            }
+            }
+
+        public void printGuess(){
+            Console.WriteLine(string.Format("{0}", string.Join(" ", guess)));       
+        }
+
+        public int compare(List<char> listPreviousGuesses, int numberOfGuesses){
+            for(int i=0;i<answer.Count;i++){
+                if (listPreviousGuesses.Contains(answer[i])){
+                    guess[i] = answer[i];}}
+            if (answer.Contains(listPreviousGuesses[numberOfGuesses-1])){
+                return 0;}
+            else {
+                return 1;}
+
+        }
+
+        public void printAnswer(){
+            Console.WriteLine(string.Format("{0}", string.Join(" ", answer)));
+        }
+
+    };
+}
